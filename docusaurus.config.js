27743abc69f99f542e3655768721d8cdba3560ca
André Vieira <a64108@ualg.pt>
2023-12-21 18:59:47 +0000

    navbar label tutorial > serviços, blog > quem somos?

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,9 +75,9 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Serviços',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/blog', label: 'Quem Somos?', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
