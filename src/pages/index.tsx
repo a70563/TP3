0199f84ce76642253d5a38ccce3dd172fe7a0a59
Inês Lidório <a70563@ualg.pt>
2023-12-22 16:40:06 +0000

    add dos contactos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/TP3/src\pages\Contactos.md">
+            to="/Contactos">
             Contacto-nos
           </Link>
         </div>
