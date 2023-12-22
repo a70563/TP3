19836eb89cefc1d8d7a5078daa0d4ef494b448f1
Inês Lidório <a70563@ualg.pt>
2023-12-22 12:26:42 +0000

    Correção do erro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/Inicio">
+            to="/docs/intro">
             Contacto-nos
           </Link>
         </div>
